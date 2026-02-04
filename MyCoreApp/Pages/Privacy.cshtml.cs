cc91941d0ac9becaeef042dc18a2a4268bbcffef
AldeaJ <jaldea1@my.centennialcollege.ca>
2026-02-03 19:36:06 -0500

    Add project files.

---
+++ MyCoreApp/Pages/Privacy.cshtml.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Globalization;
+
+namespace MyCoreApp.Pages
+{
+    public class PrivacyModel : PageModel
+    {
+        public void OnGet()
+        {
+            string dateTime = DateTime.Now.ToString("d", new CultureInfo("en-US"));
+            ViewData["TimeStamp"] = dateTime;
+        }
+    }
+
+}
